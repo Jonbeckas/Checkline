8758d0a261a7da7fa36e8767ab86eca858494538
Jonbeckas <Jonasbeckerx3@outlook.de>
2019-12-15 00:06:04 +0100

    sidebar module

---
+++ frontend/src/app/sidebar/sidebar.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LolComponent } from './lol/lol.component';
+
+
+
+@NgModule({
+  declarations: [LolComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SidebarModule { }
