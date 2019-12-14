c0661c51349cf42edc5c1ad8e768f930c1e48633
Jonbeckas <Jonasbeckerx3@outlook.de>
2019-12-15 00:02:22 +0100

    add login page

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('username', null) username;
+  @ViewChild('password', null) password;
+  @ViewChild('submit', null) submit;
 
   constructor() { }
 
