8621141230962fdeea5d716e04771ec74629cdfe
Jonbeckas <jonasbeckerx3@outlook.de>
2021-09-10 23:32:51 +0200

    feat: ci fixes

--- backend/src/modules/runners/runner-card.spec.ts
+++ backend/src/modules/runners/runner-card.spec.ts
@@ -2,12 +2,5 @@ import * as QRCode  from "qrcode"
 
 describe('RunnerCard', () => {
 
-    it("Test",async ()=> {
-        try {
-            console.log(await QRCode.toDataURL("JUST A TEST"))
-        } catch (err) {
-            console.error(err)
-        }
-    })
 
 })
