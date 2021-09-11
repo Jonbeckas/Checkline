668b835ef46f1f60b1efc0544f4d85710c9af9f4
Jonbeckas <jonasbeckerx3@outlook.de>
2021-09-11 09:46:13 +0200

    feat: add backend build system

---
+++ backend/gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require("gulp")
+var ts = require("gulp-typescript")
+var tsProject = ts.createProject("tsconfig.json")
+var uglify = require("gulp-uglify");
+var sourcemaps = require("gulp-sourcemaps");
+const {task} = require("gulp");
+const {readFileSync, writeFileSync} = require("fs");
+
+function loadJson (path) {
+    try {
+        const str = readFileSync(path).toString();
+        return JSON.parse(str);
+    } catch {
+        throw Error("Unable to load module.json")
+    }
+}
+
+function copyNewMainfest() {
+    let manifest = loadJson("package.json")
+    let newManifest = {name: "chackline-backend", version: manifest.version, dependencies: manifest.dependencies}
+    let manifesString = JSON.stringify(newManifest)
+    writeFileSync("build/package.json", manifesString)
+}
+
+
+
+
+task("default", function () {
+
+    gulp.src("src/fonts/**/*").pipe(gulp.dest("build/fonts"))
+
+    copyNewMainfest()
+
+    return tsProject.src()
+        .pipe(tsProject()).js
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest("build"))
+})
+
+
+
+
